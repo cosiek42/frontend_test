d305910b03651617b4dcecaeceba170249485818
Miłosz Śpiewak <mil-s@o2.pl>
2024-07-17 20:31:32 +0200

    Pierwszy commit

---
+++ script.js
@@ -0,0 +1,86 @@
+const rave_json = '{"books":[{"title":"W pustyni i w puszczy","portion":"To powiedziawszy wziął dziewczynkę za rękę i poszli ku pierwszej za Port-Saidem kanałowej przystani, za nimi zaś nadążała Murzynka Dinah, niegdyś piastunka małej Nel. Szli wałem oddzielającym wody jeziora Menzaleh od kanału, przez który przepływał w tej chwili, prowadzony przez pilota, duży parowiec angielski. Przyroda nieożywiona, Pustynia, Słońce, ŚwiatłoZbliżał się wieczór. Słońce stało jeszcze dość wysoko, ale przetoczyło się już na stronę jeziora. Słonawe jego wody poczynały lśnić złotem i drgać odblaskami pawich piór. Po arabskim brzegu ciągnęła się, jak okiem sięgnąć, płowa piaszczysta pustynia — głucha, złowroga, martwa. Między szklanym, jakby obumarłym niebem a bezmiarem pomarszczonych piasków nie było śladu żywej istoty. Podczas gdy na kanale wrzało życie, kręciły się łodzie, rozlegały się świsty parowców, a nad Menzaleh migotały w słońcu stada mew i dzikich kaczek — tam, na arabskim brzegu, była jakby kraina śmierci. Tylko w miarę jak słońce zniżając się stawało się coraz czerwieńsze, piaski poczęły przybierać barwę liliową, taką, jaką jesienią mają wrzosy w polskich lasach.Dzieci idąc ku przystani ujrzały jeszcze kilka czerwonaków, do których śmiały się ich oczy, po czym Dinah oświadczyła, że Nel musi wracać do domu. W Egipcie po dniach, które nawet w czasie zimy często bywają upalne, następują noce bardzo zimne, a że zdrowie Nel wymagało wielkiej ostrożności, ojciec jej, pan Rawlison, nie pozwalał, by dziewczynka znajdowała się po zachodzie słońca nad wodą. Zawrócili więc ku miastu, na którego krańcu stała w pobliżu kanału willa pana Rawlisona — i w chwili gdy słońce zanurzyło się w morzu, znaleźli się pod dachem. Niebawem przybył też zaproszony na obiad inżynier Tarkowski, ojciec Stasia — i całe towarzystwo, wraz z Francuzką, nauczycielką Nel, panią Olivier, zasiadło do stołu."},{"title":"Ogniem i mieczem","portion":"Rok 1647 był to dziwny rok, w którym rozmaite znaki na niebie i ziemi zwiastowały jakoweś klęski i nadzwyczajne zdarzenia. Współcześni kronikarze wspominają, iż z wiosny szarańcza w niesłychanej ilości wyroiła się z Dzikich Pól[1] i zniszczyła zasiewy i trawy, co było przepowiednią napadów tatarskich. Latem zdarzyło się wielkie zaćmienie słońca, a wkrótce potem kometa pojawiła się na niebie. W Warszawie widywano też nad miastem mogiłę i krzyż ognisty w obłokach; odprawiano więc posty i dawano jałmużny, gdyż niektórzy twierdzili, że zaraza spadnie na kraj i wygubi rodzaj ludzki. ZimaNareszcie zima nastała tak lekka, że najstarsi ludzie nie pamiętali podobnej. W południowych województwach lody nie popętały[2] wcale wód, które podsycane topniejącym każdego ranka śniegiem wystąpiły z łożysk i pozalewały brzegi. Padały częste deszcze. Step rozmókł i zmienił się w wielką kałużę, słońce zaś w południe dogrzewało tak mocno, że — dziw nad dziwy! — w województwie bracławskim[3] i na Dzikich Polach zielona ruń[4] okryła stepy i rozłogi[5] już w połowie grudnia. Roje po pasiekach poczęły się burzyć i huczeć, bydło ryczało po zagrodach. Gdy więc tak porządek przyrodzenia zdawał się być wcale odwróconym, wszyscy na Rusi[6], oczekując niezwykłych zdarzeń, zwracali niespokojny umysł i oczy szczególniej ku Dzikim Polom, od których łatwiej niźli skądinąd mogło się ukazać niebezpieczeństwo. Tymczasem na Polach nie działo się nic nadzwyczajnego i nie było innych walk i potyczek jak te, które się odprawiały tam zwykle, a o których wiedziały tylko orły, jastrzębie, kruki i zwierz polny."},{"title":"Pan Wołodyjowski","portion":"Po wojnie węgierskiej, po której odbył się ślub pana Andrzeja Kmicica z panną Aleksandrą Billewiczówną, miał także wstąpić w związki małżeńskie z panną Anną Borzobohatą Krasieńską równie sławny i zasłużony w Rzeczypospolitej kawaler — pan Jerzy Michał Wołodyjowski, pułkownik chorągwi laudańskiej.Ale przyszły znaczne mitręgi[1], które sprawę opóźniły i przewlokły. Panna Borzobohata była wychowanicą księżnej Jeremiowej Wiśniowieckiej[2], bez której pozwolenia żadną miarą na wesele zgodzić się nie chciała, musiał więc pan Michał pannę w Wodoktach z powodu niespokojnych czasów zostawić, a sam do Zamościa po pozwolenie i błogosławieństwo jechać. Lecz nie świeciła mu pomyślna gwiazda, gdyż księżnej w Zamościu nie zastał, która dla edukacji syna do Wiednia na dwór cesarski się udała. Wytrwały rycerz podążył za nią i do Wiednia, choć mu to siła[3] czasu zabrało. Tam załatwiwszy szczęśliwie sprawy, z dobrą otuchą do ojczyzny powracał. Czasy, wróciwszy, zastał niespokojne; wojsko do związku szło, na Ukrainie bunty trwały — od wschodniej ściany nie gasł pożar. Zaciągano nowe wojska, aby choć jako tako granice osłonić. Patriota, MiłośćZanim więc pan Michał z powrotem do Warszawy dojechał, zastał listy zapowiednie na jego imię z ramienia wojewody ruskiego wydane. Uważając zaś, że ojczyzna zawsze przed prywatą iść powinna, myśli o prędkim weselu poniechał, a na Ukrainę ruszył. Kilka lat w tamtych stronach wojował mając zaledwie sposobną porę list od czasu do czasu do utęsknionej panienki posłać, żyjąc w ogniu, w niewypowiedzianych trudach i pracy."},{"title":"Potop","portion":"Był na Żmudzi[1] ród możny Billewiczów, od Mendoga[2] się wywodzący, wielce skoligacony[3] i w całym Rosieńskiem[4] nad wszystkie inne szanowany. Do urzędów wielkich nigdy Billewiczowie nie doszli, co najwięcej powiatowe piastując, ale na polu Marsa[5] niepożyte krajowi oddali usługi, za które różnymi czasami hojnie bywali nagradzani. Gniazdo ich rodzinne istniejące do dziś zwało się także Billewicze, ale prócz nich posiadali wiele innych majętności i w okolicy Rosień, i dalej ku Krakinowu[6], wedle Laudy, Szoi, Niewiaży[7] — aż hen, jeszcze za Poniewieżem[8]. Potem rozpadli się na kilka domów, których członkowie potracili się z oczu. Zjeżdżali się wszyscy wówczas tylko, gdy w Rosieniach, na równinie zwanej Stany, odbywał się popis pospolitego ruszenia żmudzkiego. Częściowo spotykali się także pod chorągwiami litewskiego komputu[9] i na sejmikach, a że byli zamożni, wpływowi, więc liczyć się z nimi musieli sami nawet wszechpotężni na Litwie i Żmudzi Radziwiłłowie. Za panowania Jana Kazimierza patriarchą wszystkich Billewiczów był Herakliusz Billewicz, pułkownik lekkiego znaku, podkomorzy upicki. Ten nie mieszkał w gnieździe rodzinnym, które dzierżył pod owe czasy Tomasz, miecznik rosieński; zaś do Herakliusza należały Wodokty, Lubicz i Mitruny, leżące w pobliżu Laudy naokół jakoby morzem ziemiami drobnej szlachty oblane. Prócz Billewiczów bowiem kilka było tylko większych domów w okolicy, jako Sołłohuby, Montwiłłowie, Schyllingowie, Koryznowie, Sicińscy (choć i drobnej braci tychże nazwisk nie brakło), zresztą całe porzecze Laudy usiane było gęsto tak zwanymi „okolicami” albo mówiąc zwyczajnie: zaściankami zamieszkałymi przez sławną i głośną w dziejach Żmudzi szlachtę laudańską."},{"title":"Quo vadis","portion":"Petroniusz[1] obudził się zaledwie koło południa i jak zwykle, zmęczony bardzo. Poprzedniego dnia był na uczcie u Nerona[2], która przeciągnęła się do późna w noc. Od pewnego czasu zdrowie jego zaczęło się psuć. Sam mówił, że rankami budzi się jakby zdrętwiały i bez możności zebrania myśli. Ale poranna kąpiel i staranne wygniatanie ciała przez wprawionych do tego niewolników przyśpieszało stopniowo obieg jego leniwej krwi, rozbudzało go, cuciło, wracało mu siły, tak że z elaeothesium[3], to jest z ostatniego kąpielowego przedziału, wychodził jeszcze jakby wskrzeszony, z oczami błyszczącymi dowcipem i wesołością, odmłodzon[4], pełen życia, wykwintny, tak niedościgniony, że sam Otho[5] nie mógł się z nim porównać, i prawdziwy, jak go nazywano: arbiter elegantiarum[6]. W łaźniach publicznych[7] bywał rzadko: chyba że zdarzył się jakiś budzący podziw retor[8], o którym mówiono w mieście, lub gdy w efebiach[9] odbywały się wyjątkowo zajmujące zapasy. Zresztą miał w swej insuli[10] własne kąpiele, które słynny wspólnik Sewerusa, Celer[11], rozszerzył mu, przebudował i urządził z tak nadzwyczajnym smakiem, iż sam Nero przyznawał im wyższość nad cezariańskimi, chociaż cezariańskie były obszerniejsze i urządzone z nierównie większym przepychem. Po owej więc uczcie, na której znudziwszy się błaznowaniem Watyniusza[12] brał wraz z Neronem, Lukanem[13] i Senecjonem[14] udział w diatrybie[15]: czy kobieta ma duszę — wstawszy późno, zażywał, jak zwykle, kąpieli. Dwaj ogromni balneatorzy[16] złożyli go właśnie na cyprysowej mensie[17], pokrytej śnieżnym egipskim byssem[18], i dłońmi, maczanymi w wonnej oliwie, poczęli nacierać jego kształtne ciało — on zaś z zamkniętymi oczyma czekał, aż ciepło laconicum[19] i ciepło ich rąk przejdzie w niego i usunie zeń znużenie."},{"title":"Krzyżacy","portion":"W Tyńcu, w gospodzie „Pod Lutym Turem”, należącej do opactwa, siedziało kilku ludzi, słuchając opowiadania wojaka bywalca, który z dalekich stron przybywszy, prawił im o przygodach, jakich na wojnie i w czasie podróży doznał. Człek był brodaty, w sile wieku, pleczysty, prawie ogromny, ale wychudły; włosy nosił ujęte w pątlik, czyli w siatkę naszywaną paciorkami; na sobie miał skórzany kubrak z pręgami wyciśniętymi przez pancerz, na nim pas, cały z miedzianych klamr; za pasem nóż w rogowej pochwie, przy boku zaś krótki kord podróżny. Tuż przy nim za stołem siedział młodzieńczyk o długich włosach i wesołym spojrzeniu, widocznie jego towarzysz lub może giermek, bo przybrany także po podróżnemu, w taki sam powyciskany od zbroicy skórzany kubrak. Resztę towarzystwa stanowiło dwóch ziemian z okolic Krakowa i trzech mieszczan w czerwonych składanych czapkach, których cienkie końce zwieszały się im z boku aż na łokcie. Gospodarz Niemiec, w płowym kapturze z kołnierzem wycinanym w zęby, lał im z konwi sytne piwo do glinianych stągiewek i nasłuchiwał ciekawie przygód wojennych. Jeszcze ciekawiej jednak słuchali mieszczanie. W owych czasach nienawiść, jaka dzieliła za czasów Łokietkowych miasto od rycerskiego ziemiaństwa, znacznie już była przygasła, mieszczaństwo zaś nosiło głowy górniej niż w wiekach późniejszych. Jeszcze ceniono ich gotowość ad concessionem pecuniarum; dlatego też nieraz zdarzało się widzieć w gospodach kupców pijących za pan brat ze szlachtą. Widziano ich nawet chętnie, bo jako ludzie, u których o gotowy grosz łatwiej, płacili zwykle za herbowych."}]}';
+const json = JSON.parse(rave_json);
+let rnd = [];
+
+document.getElementById("replace").addEventListener("click", function() {
+	let r1 = document.getElementById("first");
+	let r2 = document.getElementById("second");
+	let r3 = document.getElementById("random");
+	let title = document.getElementById("tag");
+	let area = document.getElementById("lorem");
+	rnd = [];
+	if (r1.checked) {
+		title.innerHTML = json.books[0].title;
+		area.innerHTML = json.books[0].portion;
+		rnd.push(0);
+	}
+	else if (r2.checked) {
+		title.innerHTML = json.books[1].title;
+		area.innerHTML = json.books[1].portion;
+		rnd.push(1);
+	}
+	else if (r3.checked) {
+		let nr = Math.floor(Math.random() * 4) + 2;
+		title.innerHTML = json.books[nr].title;
+		area.innerHTML = json.books[nr].portion;
+		rnd.push(nr);
+	}
+});
+
+document.getElementById("join").addEventListener("click", function() {
+	let r1 = document.getElementById("first");
+	let r2 = document.getElementById("second");
+	let r3 = document.getElementById("random");
+	let area = document.getElementById("lorem");
+	if (r1.checked) {
+		if (rnd.includes(0)) alert("Opcja pierwsza jest już doklejona!");
+		else {
+			area.innerHTML += "<br>" + json.books[0].portion;
+			rnd.push(0);
+		}
+	}
+	if (r2.checked) {
+		if (rnd.includes(1)) alert("Opcja druga jest już doklejona!");
+		else {
+			area.innerHTML += "<br>" + json.books[1].portion;
+			rnd.push(1);
+		}
+	}
+	if (r3.checked) {
+		if (rnd.includes(2) && rnd.includes(3) && rnd.includes(4) && rnd.includes(5)) alert("Wszystkie opcje zostały doklejone!");
+		else {
+			let nr;
+			do nr = Math.floor(Math.random() * 4) + 2;
+			while (rnd.includes(nr));
+			area.innerHTML += "<br>" + json.books[nr].portion;
+			rnd.push(nr);
+		}
+	}
+});
+
+document.getElementById("showbut2").addEventListener("click", function() {
+	var menu = document.getElementById("menu");
+	if (menu.style.display === "block") menu.style.display = "none";
+	else menu.style.display = "block";
+});
+
+document.getElementById("reset").addEventListener("click", function() {
+	let text = document.getElementById("head-text");
+	let title = document.getElementById("tag");
+	let area = document.getElementById("lorem");
+	if (text.innerHTML.includes("Miłosz")) {
+		text.innerHTML = "Zadanie <b>rekrutacyjne</b>";
+		text.style.paddingTop = "15px";
+	}
+	title.innerHTML = "BLOK Z DŁUGĄ NAZWĄ KTÓRA SAMA SIĘ PRZYTNIE ...";
+	area.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec cursus diam eget ex tincidunt hendrerit. Fusce sodales fermentum cursus. Proin arcu lectus, pulvinar id facilisis sed, consequat in sem. Donec egestas, felis hendrerit varius varius, massa turpis sodales metus, in ornare diam est in lorem. Quisque sit amet tincidunt eros, quis imperdiet lacus. Integer at magna nulla. Suspendisse placerat ex vitae purus ultrices euismod. Morbi eleifend, nisl et cursus maximus, sapien mauris malesuada turpis, a ullamcorper magna tellus pharetra libero. Mauris maximus in ipsum eu semper. Pellentesque ullamcorper ultricies ante at finibus. Suspendisse quis lectus porttitor, dapibus eros blandit, condimentum libero. Phasellus auctor magna quis quam varius consequat. Morbi ullamcorper pulvinar velit vehicula lobortis. Suspendisse nec est in ex dignissim pellentesque. Nunc condimentum malesuada elit tincidunt eleifend. Fusce ut tristique odio. Morbi semper lectus vitae nisi fermentum, nec sollicitudin dui dapibus. Pellentesque et sapien eget erat eleifend lacinia id vitae diam. Mauris condimentum, lacus id pellentesque finibus, eros nulla facilisis ligula, at porttitor nisi enim vel erat. Etiam at nulla mauris. Proin consectetur lectus diam, ut tincidunt sapien auctor interdum. Aliquam eget finibus massa. Vestibulum blandit accumsan nisl nec elementum. Vivamus vel mauris sed orci mollis venenatis eget at lectus.";
+	rnd = [];
+});
+
+document.getElementById("show").addEventListener("click", function() {
+	let text = document.getElementById("head-text");
+	if (text.innerHTML.includes("Miłosz") == false) {
+		text.innerHTML += "<br>Miłosz Śpiewak";
+		text.style.paddingTop = "5px";
+	}
+});
